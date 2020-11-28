e1b4d8cb870698ebd806d49a05aad7d6389be41f
Vovchek <Vlad.aks03@mail.ru>
2020-11-28 22:36:51 +0500

    Карта работает, файлы добавил

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
+import pygame as pg
+from map import *
+from settings import *
 
 
 def main():
-    pass
+    global screen
+    pg.init()
+    screen = pg.display.set_mode((size[0] * rect_size2d,
+                                 size[1] * rect_size2d))
+
+    draw_map()
+
+    pg.display.flip()
+    while pg.event.wait().type != pg.QUIT:
+        pass
+    pg.quit()
+
+
+def draw_map():
+    for i in range(len(map_)):
+        for j in range(len(map_[i])):
+            if map_[i][j] == '#':
+                pg.draw.rect(screen, rect_color2d, (j * rect_size2d, i * rect_size2d, rect_size2d, rect_size2d), 4)
 
 
 if __name__ == '__main__':
---
+++ settings.py
@@ -0,0 +1,6 @@
+rect_size2d = 48
+size = 32, 16
+rect_color2d = (255, 255, 255)
+
+
+
