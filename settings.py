b09bc3e3b5879d46ccdaf7c4ae74e678b0eb746c
YerkhovaAnastasia <nerhova26@gmail.com>
2020-12-22 18:55:05 +0500

    Пояснения для переменных

--- settings.py
+++ settings.py
@@ -4,21 +4,19 @@ width = 1920
 height = 1080
 
 size = 32, 16
-half_size = size[0] // 2, size[1] // 2
-rect_size2d = width / size[0]
+half_size = size[0] // 2, size[1] // 2  # середина карты
+rect_size2d = width / size[0]  # размер квадрата на карте
 
-draw_dist = 1500
+draw_dist = 1500  # длина луча
 
-fow = 60
-lines = 100
-line_step = fow / lines / 50
-line_to_px = width / lines
+fow = 60  # угол прорисовки
+lines = 100  # кол-во линий
+line_step = fow / lines / 50  # угол между 2-мя лучами
+line_to_px = width / lines  # ширина у 3д стены
 
-bese_wall_h = 300
+bese_wall_h = 300  # высота у 3д стены
 
 white = (255, 255, 255)
 green = (0, 255, 0)
 red = (255, 0, 0)
 gray = (128, 128, 128)
-
-
