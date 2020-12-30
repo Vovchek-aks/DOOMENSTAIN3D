0348b36b2d494ea2d92faea368f1f8fa04424138
Vovchek <Vlad.aks03@mail.ru>
2020-12-30 12:07:57 +0500

    вроде не сломал

--- main.py
+++ main.py
@@ -3,6 +3,24 @@ from map import *
 from settings import *
 from player import Player
 import math
+import sys
+import os
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', 'sprites', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pg.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
 
 
 def raycast(sc, player):
