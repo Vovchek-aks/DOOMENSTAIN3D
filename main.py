830b80d2d86d08eca1790109fa8da087685cc8bd
Vovchek <Vlad.aks03@mail.ru>
2021-01-13 21:31:57 +0500

    -

--- main.py
+++ main.py
@@ -298,47 +298,6 @@ class Spr(GameObject):
         self.base_im = self.image = spr
 
 
-def start_screen():
-    sc = pg.display.set_mode((width, height))
-    fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
-    but_menu = load_image('levels.png')
-    sc.blit(fon, (0, 0))
-    sc.blit(but_menu, (width / 4, height / 4))
-    pygame.font.get_fonts()
-    quitt = load_image('quit.png')
-    sc.blit(quitt, (width // 4, height // 2))
-    clock = pygame.time.Clock()
-    running = True
-    rect_b_lv = [width / 3.74, height / 3.6]
-    rect_b_quit = [width / 3.74, height / 1.9]
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pg.KEYDOWN:
-                if event.key == pg.K_ESCAPE:
-                    running = False
-                    exit(0)
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.pos[0] >= rect_b_lv[0] and event.pos[1] >= rect_b_lv[1]:
-                    if event.pos[0] <= rect_b_lv[0] + but_menu.get_rect().w and event.pos[1] >= rect_b_lv[1]:
-                        if event.pos[0] <= rect_b_lv[0] + but_menu.get_rect().w and \
-                                event.pos[1] <= rect_b_lv[1] + but_menu.get_rect().h:
-                            if event.pos[0] >= rect_b_lv[0] and event.pos[1] <= rect_b_lv[1] + but_menu.get_rect().h:
-                                return
-                if event.pos[0] >= rect_b_quit[0] and event.pos[1] >= rect_b_quit[1]:
-                    if event.pos[0] <= rect_b_quit[0] + but_menu.get_rect().w and event.pos[1] >= rect_b_quit[1]:
-                        if event.pos[0] <= rect_b_quit[0] + but_menu.get_rect().w and \
-                                event.pos[1] <= rect_b_quit[1] + but_menu.get_rect().h:
-                            if event.pos[0] >= rect_b_quit[0] and \
-                                    event.pos[1] <= rect_b_quit[1] + but_menu.get_rect().h:
-                                pygame.quit()
-                                sys.exit()
-        pygame.display.flip()
-        clock.tick(FPS)
-
-
 def raycast(player):
     ret = []
     for i in range(lines):
@@ -708,7 +667,8 @@ def main():
                         if event.pos[0] <= rect_b_menu[0] + menu.get_rect().w and event.pos[1] >= rect_b_menu[1]:
                             if event.pos[0] <= rect_b_menu[0] + menu.get_rect().w and \
                                     event.pos[1] <= rect_b_menu[1] + menu.get_rect().h:
-                                if event.pos[0] >= rect_b_menu[0] and event.pos[1] <= rect_b_menu[1] + menu.get_rect().h:
+                                if event.pos[0] >= rect_b_menu[0] and event.pos[1] <= rect_b_menu[
+                                    1] + menu.get_rect().h:
                                     start_screen()
 
             if (player.pos, player.ang) != ppos:
