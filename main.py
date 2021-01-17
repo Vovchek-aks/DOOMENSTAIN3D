84021df8845180d95bd4f0ed5298a2c62e468952
YerkhovaAnastasia <nerhova26@gmail.com>
2021-01-17 20:29:14 +0500

    павука жалко... помянем

--- main.py
+++ main.py
@@ -730,7 +730,7 @@ def main():
 
     obj_spr = {Door: load_image('дверь.png'),
                Spider: [load_image('321.png'),
-                        load_image('321.png')],
+                        load_image('321 ранен(.png')],
                Zombie: [load_image('zombie.png'),
                         load_image('zombie2.png')],
                Key: load_image('ключ.png'),
